283d09abb6f2f1173b9716f5b661afaeba2bdb0a
sarbajitmohanty <sarbajit.mohanty02@gmail.com>
2021-05-27 00:15:31 +0530

    add banner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
+import Banner from "./Banner";
 
 //9fa97ce22b1157cfeb46a1c36c86a366
 //https://api.themoviedb.org/3/movie/550?api_key=9fa97ce22b1157cfeb46a1c36c86a366
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className="app">
       {/* Nav */}
-      {/* Banner */}
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOroginals}
