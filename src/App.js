794e7e461197025f8ce72a8eab0a3b65f5fe92b6
sarbajitmohanty <sarbajit.mohanty02@gmail.com>
2021-05-26 20:47:09 +0530

    get movies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Row from './Row';
+import requests from './requests';
 
 //9fa97ce22b1157cfeb46a1c36c86a366
 //https://api.themoviedb.org/3/movie/550?api_key=9fa97ce22b1157cfeb46a1c36c86a366
@@ -8,6 +10,8 @@ function App() {
   return (
     <div className="App">
       <h1>Netflix-Clone</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOroginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
